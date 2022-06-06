9576820a63535a60c0623ed8e40b3b3f95a92da0
andresho <andre8hoffmann@gmail.com>
2022-06-06 19:16:44 -0300

    feat: add escape_time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use num::Complex;
+
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+ 
+    None
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let a = escape_time(Complex{
+        im: 0.1,
+        re: 0.2
+    }, 1000);
+
+    match a {
+        Some(x) => println!("value: {}!", x),
+        None => println!("value: None!")
+    }
 }
