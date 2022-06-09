d66563edaf473f5d6352090e2bce86ed4dc77027
andresho <andre8hoffmann@gmail.com>
2022-06-08 22:25:21 -0300

    fix: rendering set

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,81 @@
-use num::Complex;
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+
+use log::error;
+use pixels::{Error, Pixels, SurfaceTexture};
 use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+use num::Complex;
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 800;
+
+struct Camera {
+    camera_zoom: f64,
+    camera_x: f64,
+    camera_y: f64,
+    velocity_x: i16,
+    velocity_y: i16,
+}
+
+fn main() -> Result<(), Error> {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Mandelbrot-set")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+    let mut camera = Camera::new();
+
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            camera.draw(pixels.get_frame());
+            if pixels
+                .render()
+                .map_err(|e| error!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+            }
+
+            // Update internal state and request a redraw
+            camera.update();
+            window.request_redraw();
+        }
+    });
+}
+
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
@@ -13,80 +89,41 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     None
 }
 
-fn render(pixels:&mut [u8], bounds: (usize, usize)) {
-    let half_bounds_0 = (bounds.0 as f64)/2.0;
-    let half_bounds_1 = (bounds.1 as f64)/2.0;
-
-    for row in 0..bounds.1 {
-        for column in 0..bounds.0 {
-            let point = Complex {
-                re: (half_bounds_0 - row as f64)/100.0,
-                im: (half_bounds_1 - column as f64)/100.0
-            };
-            pixels[row * bounds.0 + column] =
-                match escape_time(point, 255) {
-                    None => 0,
-                    Some(count) => 255 - count as u8
-                };
+impl Camera {
+    fn new() -> Self {
+        Self {
+            camera_zoom: 0.6,
+            camera_x: 0.0,
+            camera_y: 0.0,
+            velocity_x: 1,
+            velocity_y: 1
         }
     }
-}
 
-use winit::{
-    event::{Event, WindowEvent, VirtualKeyCode},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-use winit_input_helper::WinitInputHelper;
-use log::error;
-use pixels::{Error, Pixels, SurfaceTexture};
+    fn update(&mut self) {
+        
+    }
 
-fn main() -> Result<(), Error> {
-    println!("Hello, world!");
+    fn draw(&self, frame: &mut [u8]) {
+        let camera_zoom = 500.0 * self.camera_zoom;
 
-    let bounds = (1000, 1000);
-    
-    let size = LogicalSize::new(bounds.0 as f64, bounds.1 as f64);
+        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+            let x = (i % WIDTH as usize) as f64 + self.camera_x;
+            let y = (i / WIDTH as usize) as f64 + self.camera_y;
 
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("mandelbrot-set")
-        .with_inner_size(size)
-        .with_min_inner_size(size)
-        .build(&event_loop)
-        .unwrap();
+            let point = Complex {
+                re: ((((WIDTH as f64)/2.0) - x as f64))/camera_zoom,
+                im: ((((WIDTH as f64)/2.0) - y as f64))/camera_zoom
+            };
 
-    let mut input = WinitInputHelper::new();
+            let color = match escape_time(point, 255) {
+                None => 100,
+                Some(count) => 255 - count as u8
+            };
 
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(bounds.0 as u32, bounds.1 as u32, surface_texture)?
-    };
+            let rgba = [color, color, color, color];
 
-    render(pixels.get_frame(), bounds);
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        // Draw the current frame
-        if let Event::RedrawRequested(_) = event {        
-            if pixels
-                .render()
-                .map_err(|e| error!("pixels.render() failed: {}", e))
-                .is_err()
-            {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
+            pixel.copy_from_slice(&rgba);
         }
-        // Handle input events
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-        }
-    });
+    }
 }
